23eb66c12d5a3888ecdc1e645d1894db0af71e63
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2022-09-25 19:24:27 +0000

    Initial commit

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,26 @@
 def arithmetic_arranger(problems):
 
+  problem_length = len(problems)
 
-    return arranged_problems
+  if problem_length > 5:
+    return "Error: Too many problems."
+  
+  for problem in problems:
+    problem = problem.split()
+    
+    operand_one = problem[0]
+    operand_two = problem[2]
+    operator = problem[1]
+
+    if operator != "+" and operator != "-":
+      return "Error: Operator must be '+' or '-'."
+
+    if len(operand_one) > 4 or len(operand_two) > 4:
+      return "Error: Numbers cannot be more than four digits."
+
+    if not operand_one.isdigit() or not operand_two.isdigit():
+      return "Error: Numbers must only contain digits."
+    
+    print("still working on it...")
+
+  # return arranged_problems
